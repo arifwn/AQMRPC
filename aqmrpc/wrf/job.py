647b38d924298410106e27b0895a7f491182dcfe
Arif Widi Nugroho <arif@sainsmograf.com>
2012-03-22 19:28:28 +0700

    progress on wrf job

--- aqmrpc/wrf/job.py
+++ aqmrpc/wrf/job.py
@@ -1,5 +1,12 @@
 '''
 '''
 
-from servercon.supervisor import BaseJob
+from servercon import supervisor
 
+class WRFRun(supervisor.BaseJob):
+    def __init__(self, name, callback=None):
+        super(WRFRun, self).__init__(name=name,
+                                     runner=supervisor.THREAD_POOL_CPUBOUND_SIZE,
+                                     callback=callback)
+
+    
