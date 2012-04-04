93d16d96460ab27027ee50c30e8c2e0b8f0f96d7
Arif Widi Nugroho <arif@sainsmograf.com>
2012-04-05 03:33:26 +0700

    added line counter command

--- fabfile.py
+++ fabfile.py
@@ -17,3 +17,77 @@ project_dir = ''
 
 def dump_data():
     local('python manage.py dumpdata --indent=4 > aqmrpc/fixtures/initial_data.json')
+
+def get_all_files(directory, ext='.py'):
+    import os
+    
+    ext_index = -len(ext)
+    file_list = []
+    
+    for root, dirs, files in os.walk(directory):
+        for f in files:
+            if f[ext_index:] == ext:
+                file_list.append(os.path.join(root, f))
+    
+    return file_list
+
+def count_line():
+    ''' Count total line number of select files.
+        find . -name '*.py' | xargs wc -l
+    '''
+    
+    from cStringIO import StringIO
+    
+    f = {}
+    name = 'aqmrpc'
+    f['%s_py' % name] = get_all_files(name, '.py')
+    f['%s_html' % name] = get_all_files(name, '.html')
+    f['%s_js' % name] = get_all_files(name, '.js')
+    f['%s_coffee' % name] = get_all_files(name, '.coffee')
+    f['%s_less' % name] = get_all_files(name, '.less')
+    
+    name = 'rest'
+    f['%s_py' % name] = get_all_files(name, '.py')
+    f['%s_html' % name] = get_all_files(name, '.html')
+    f['%s_js' % name] = get_all_files(name, '.js')
+    f['%s_coffee' % name] = get_all_files(name, '.coffee')
+    f['%s_less' % name] = get_all_files(name, '.less')
+    
+    name = 'servercon'
+    f['%s_py' % name] = get_all_files(name, '.py')
+    f['%s_html' % name] = get_all_files(name, '.html')
+    f['%s_js' % name] = get_all_files(name, '.js')
+    f['%s_coffee' % name] = get_all_files(name, '.coffee')
+    f['%s_less' % name] = get_all_files(name, '.less')
+    
+    exclude_list = []
+    
+    file_list_nonfiltered = []
+    file_list = []
+    for key, val in f.iteritems():
+        if len(val) > 0:
+            file_list_nonfiltered.extend(val)
+    
+    for f in file_list_nonfiltered:
+        skip = False
+        for exclude in exclude_list:
+            if exclude in f:
+                skip = True
+        if skip:
+            continue
+        file_list.append(f)
+    
+    file_list_buffer = StringIO()
+    
+    file_list_buffer.write('wc -l ')
+    
+    for f in file_list:
+        file_list_buffer.write(f)
+        file_list_buffer.write(' ')
+        
+    cmd = file_list_buffer.getvalue()
+    
+    if len(file_list) > 0:
+        local(cmd)
+    
+    
