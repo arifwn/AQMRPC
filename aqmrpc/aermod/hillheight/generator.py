f4af7d3579cdb3df3db7d75f7f37a3c2ac67f5a8
Arif Widi Nugroho <arif@sainsmograf.com>
2012-04-21 06:15:26 +0700

    added skeleton methods

--- aqmrpc/aermod/hillheight/generator.py
+++ aqmrpc/aermod/hillheight/generator.py
@@ -3,8 +3,19 @@ import numpy
 
 
 class ModelGrid(object):
-    def __init__(self, pos_x, pos_y, delta_x, delta_y, grid_w, grid_h, elev_add):
-        pass
+    def __init__(self, x, y, delta_x, delta_y, w, h, base_elevation=0):
+        self.x = x
+        self.y = y
+        self.delta_x = delta_x
+        self.delta_y = delta_y
+        self.w = w
+        self.h = h
+        self.base_elevation = base_elevation
+        self.hillheight = numpy.array([])
+        self.hillheight.resize((w, h))
+    
+    def set_hillheight(self, x, y, hillheight):
+        self.hillheight[x][y] = self.base_elevation + hillheight
 
 
 class HillHeight(object):
@@ -12,7 +23,7 @@ class HillHeight(object):
         self.srtm_files = []
         self.srtm_pos = []
         self.srtm_data = []
-        self.model_data = []
+        self.model_grids = []
         self.ref_lat = ref_lat
         self.ref_lon = ref_lon
     
@@ -32,9 +43,9 @@ class HillHeight(object):
         self.srtm_pos.append((lat, lon))
         self.srtm_data.append(hgt_data)
     
-    def add_model_grid(self, pos_x, pos_y, delta_x, delta_y, grid_w, grid_h, elev_add):
-        model_grid = ModelGrid(pos_x, pos_y, delta_x, delta_y, grid_w, grid_h, elev_add)
-        self.model_data.append(model_grid)
+    def add_model_grid(self, x, y, delta_x, delta_y, w, h, base_elevation=0):
+        model_grid = ModelGrid(x, y, delta_x, delta_y, w, h, base_elevation)
+        self.model_grids.append(model_grid)
     
     def srtm_get_distance_x(self, srtm_index, x_pos):
         '''
@@ -68,8 +79,18 @@ class HillHeight(object):
         ''' Return elevation for particular point in model grid. '''
     
     def process(self):
-        pass
+        ''' Compute hillheight data from all srtm data. '''
+        for grid in self.model_grids:
+            for x in xrange(grid.w):
+                for y in xrange(grid.h):
+                    # compute hillheight from srtm data
+                    hillheight = 0
+                    grid.set_hillheight(x, y, hillheight)
+        
     
     def save(self, target_path):
-        pass
+        '''
+        Save to intermediary format to be used for AERMOD runner.
+        JSON maybe?
+        '''
     
